bc4e8c928a303233e35cf4bdaf6fb807ca8b65b9
shiam <shiamabdullah1@gmail.com>
2021-03-11 23:51:07 +0600

    about us created

---
+++ view/aboutUs.php
@@ -0,0 +1,22 @@
+<?php 
+    $title="About Us";
+	include('header.php');
+?>
+
+<table>
+    <tr>
+        <h1>Welcome to MAB </h1>
+    </tr>
+    <tr>
+        <h4>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam facilis eum dolor est iste molestiae laboriosam dolorem error obcaecati consequatur expedita magni, illum iusto voluptate porro, fugit quas. Facere, sunt! 
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium repudiandae, quam nam debitis, pariatur animi sit architecto eos corrupti dolorem accusamus modi explicabo a beatae eaque aspernatur! Inventore, corrupti nobis?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium repudiandae, quam nam debitis, pariatur animi sit architecto eos corrupti dolorem accusamus modi explicabo a beatae eaque aspernatur! Inventore, corrupti nobis?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium repudiandae, quam nam debitis, pariatur animi sit architecto eos corrupti dolorem accusamus modi explicabo a beatae eaque aspernatur! Inventore, corrupti nobis?
+        </h4>
+    </tr>
+</table>
+
+<?php 
+include('footer.php');
+ ?>
