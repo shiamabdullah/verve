d31a6c2ddfe84bd5863c294a23f0ab87ec1725cd
shiam <shiamabdullah1@gmail.com>
2021-03-13 00:05:04 +0600

    index edited and randomized

--- view/login.php
+++ view/login.php
@@ -9,7 +9,7 @@
 <body>
 	<ul>    
         <li><a href="loginAdmin.php"> Admin</a> </li>
-        <li><a href="indexCus.php"> Customer</a></li>
+        <li><a href="loginCus.html"> Customer</a></li>
         <li><a href="employeesignup.php"> Employee</a></li>
     </ul>
 </body>
--- view/signup.php
+++ view/signup.php
@@ -5,7 +5,7 @@
 
 <ul>
     <li><a href="signupAdmin.php"> Admin</a> </li>
-    <li><a href="signupAdmin.php"> User</a></li>
+    <li><a href="signupCus.html"> Customer</a></li>
     <li><a href="employeesignup.php"> Employee</a></li>
 </ul>
 <!-- <a href="signup.php"> Signup</a>
